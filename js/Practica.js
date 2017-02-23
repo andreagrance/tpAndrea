d0c2d48fc8880be96021de0151d0e2170940b29c
andreagrance <andrea_salinasgrance@hotmail.com>
2017-02-23 14:03:05 -0300

    lista desplegable

---
+++ js/Practica.js
@@ -0,0 +1,12 @@
+function miFuncion(){
+    document.getElementById('jav_ext').innerHTML= "javascript externo";
+}
+var x,y, z;
+x=10;
+y=28;
+z=x+y;
+function sumar(){
+    document.getElementById("id_z").innerHTML=z;
+}
+
+document.getElementById("deci").innerHTML= 10.50;
